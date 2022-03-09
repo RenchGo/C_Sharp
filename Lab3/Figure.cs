4dc0c02a19755d1a4aef55726082e2552a700766
RenchGo <94788714+RenchGo@users.noreply.github.com>
2022-03-09 23:41:42 +0300

    Add files via upload

---
+++ Lab3/Figure.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace Lab3
+{
+    public abstract class Figure
+    {
+        public string Name { get; set; }
+        public System.Drawing.Color Color { get; set; }
+        public System.Drawing.Point Position { get; set; }
+        public System.Drawing.Point Position1 { get; set; }
+        public System.Drawing.Point Position2 { get; set; }
+        public abstract double GetArea();
+        public abstract System.Drawing.Point GetCenter();
+        public abstract void Draw(Graphics gr);
+    }
+
+    class Rectangle : Figure { //Прямоугольник
+        public double Width, Height;
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+        public override Point GetCenter()
+        {
+            return new Point((int)(Position.X + Width / 2), (int)(Position.Y + Height / 2));
+        }
+        public override void Draw(Graphics gr)
+        {
+            gr.DrawRectangle(new Pen(Color), Position.X, Position.Y, (int)Width, (int)Height);
+            gr.DrawString(GetArea().ToString(), new Font("Arial", 9), Brushes.Black, GetCenter());
+        }
+    }
+    class Circle: Figure //Круг
+    {
+        public double R;
+        public override double GetArea()
+        {
+            return Math.PI * R * R;
+        }
+        public override Point GetCenter()
+        {
+            return new Point((int)(Position.X + R / 2), (int)(Position.Y + R / 2));
+        }
+        public override void Draw(Graphics gr)
+        {
+            gr.DrawEllipse(new Pen(Color), Position.X, Position.Y, (int)R*2, (int)R*2);
+            gr.DrawString(GetArea().ToString(), new Font("Arial", 9), Brushes.Black, GetCenter());
+
+        }
+    }
+    class Square : Figure //Квадрат
+    {
+        public double Width, Height;
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+        public override Point GetCenter()
+        {
+            return new Point((int)(Position.X + Width / 2), (int)(Position.Y + Height / 2));
+        }
+        public override void Draw(Graphics gr)
+        {
+            gr.DrawRectangle(new Pen(Color), Position.X, Position.Y, (int)Width, (int)Height);
+            gr.DrawString(GetArea().ToString(), new Font("Arial", 9), Brushes.Black, GetCenter());
+        }
+    }
+    class Triangle : Figure  //Треугольник
+    {
+        public double Footing, Height;
+        
+        public override double GetArea()
+        {
+            return 0.5 * Footing * Height;
+        }
+        public override Point GetCenter()
+        {
+            return new Point((int)(Position.X + Footing / 2), (int)(Position.Y + Height / 2));
+        }
+        public override void Draw(Graphics gr)
+        {
+            Point[] points = new Point[3];
+            points[0] = new Point((int)Position.X + 0, (int)Position.Y + 0);
+            points[1] = new Point((int)Position.X + 0, (int)Position.Y + (int)this.Height);
+            points[2] = new Point((int)Position.X + (int)this.Footing, Position.Y + (int)this.Height);
+            gr.DrawPolygon(new Pen(Color),points);
+            gr.DrawString(GetArea().ToString(), new Font("Arial", 9), Brushes.Black, GetCenter());
+        }
+    }/*
+    class Trapezoid : Figure //Трапеция
+    {
+        public double Height, Side_1, Side_2;
+        public override double GetArea()
+        {
+            return 0.5 * Height * (Side_1 + Side_2);
+        }
+    }
+    class Rhomb : Figure //Ромб
+    {
+        public double Footing, Height;
+        public override double GetArea()
+        {
+            return Footing * Height;
+        }
+    }
+    class Parallelogram : Figure //Параллелограмм
+    {
+        public double Footing, Height;
+        public override double GetArea()
+        {
+            return Footing * Height;
+        }
+    }
+    class  Regular_pentagon : Figure //Правильный пятиугольник
+    {
+        public double PI = Math.PI/5, R;
+        public override double GetArea()
+        {
+            return 5 * R * R * Math.Tan(PI);
+        }
+    }
+    class Regular_decagon : Figure //Правильный десятиугольник
+    {
+        public double PI = Math.PI / 10, R;
+        public override double GetArea()
+        {
+            return 10 * R * R * Math.Tan(PI);
+        }
+    }*/
+}
---
+++ Lab3/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лаб-ная №3 - Полиморфизм\nВыполнил - Подолянко Владимир\n");
+
+
+            Rectangle a = new Rectangle()
+            {
+                Name = "Прямоугольник",
+                Width = 150,
+                Height = 60,
+                Color = System.Drawing.Color.DarkRed,
+                Position = new System.Drawing.Point(30, 30)
+            };
+            /*Rectangle b = new Rectangle() {
+                Name = "Прямоугольник Б", Width = 7.9, Height = 2.6
+            };
+
+            Console.WriteLine("Название фигуры = {0}\nПлощадь фигуры = {1}\n", a.Name, a.GetArea());*/
+            Console.WriteLine("Фигура = {0}\nПлощадь = {1}\nЦвет = {2}\nПоложение фигруы = {3}\nКоординаты центра = {4}", a.Name, a.GetArea(), a.Color, a.Position, a.GetCenter());
+
+            
+            Circle c = new Circle()
+            {
+                Name = "Круг", R = 50,
+                Color = System.Drawing.Color.Blue,
+                Position = new System.Drawing.Point(50, 250)
+            };
+            Console.WriteLine("Фигура = {0}\nПлощадь = {1}\nЦвет = {2}\nПоложение фигруы = {3}\nКоординаты центра = {4}", c.Name, c.GetArea(), c.Color, c.Position, c.GetCenter());
+
+
+            Square d = new Square()
+            {
+                Name = "Квадрат", Width = 100, Height = 100,
+                Color = System.Drawing.Color.DarkGreen,
+                Position = new System.Drawing.Point(50, 120)
+            };
+            Console.WriteLine("Фигура = {0}\nПлощадь = {1}\nЦвет = {2}\nПоложение фигруы = {3}\nКоординаты центра = {4}", d.Name, d.GetArea(), d.Color, d.Position, d.GetCenter());
+
+
+            Triangle e = new Triangle()
+            {
+                Name = "Треугольник Е", Footing = 50, Height = 100,
+                Color = System.Drawing.Color.Orange,
+                Position = new System.Drawing.Point(50, 450),
+            };
+            Console.WriteLine("Название фигуры = {0}\nПлощадь фигуры = {1}\n", e.Name, e.GetArea());
+
+            /*
+             Trapezoid f = new Trapezoid()
+            {
+                Name = "Трапеция Ф", Height = 10, Side_1=5, Side_2=10
+            };
+            Console.WriteLine("Название фигуры = {0}\nПлощадь фигуры = {1}\n", f.Name, f.GetArea());
+
+
+             Rhomb u = new Rhomb()
+            {
+                Name = "Ромб У", Footing = 12, Height = 20
+            };
+            Console.WriteLine("Название фигуры = {0}\nПлощадь фигуры = {1}\n", u.Name, u.GetArea());
+
+
+            Rhomb k = new Rhomb()
+            {
+                Name = "Параллелограмм К", Footing = 13, Height = 18
+            };
+            Console.WriteLine("Название фигуры = {0}\nПлощадь фигуры = {1}\n", k.Name, k.GetArea());
+
+
+             Regular_pentagon l = new Regular_pentagon()
+            {
+                Name = "Правильный пятиугольник К", R=10
+            };
+            Console.WriteLine("Название фигуры = {0}\nПлощадь фигуры = {1}\n", l.Name, l.GetArea());
+
+
+            Regular_decagon m = new Regular_decagon()
+            {
+                Name = "Правильный десятиугольник М",
+                R = 15
+            };
+            Console.WriteLine("Название фигуры = {0}\nПлощадь фигуры = {1}\n", m.Name, m.GetArea());*/
+
+            Form frm = new Form()
+            {
+                Text = "Лабораторная №3 - Полиморфизм",
+                Size = new System.Drawing.Size(800, 600),
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            frm.Paint += Frm_Paint;
+            Application.Run(frm);
+        }
+        private static void Frm_Paint(object sender, PaintEventArgs e) {
+            foreach (Figure f in figures) {
+                f.Draw(e.Graphics);
+            }
+        }
+        public static Figure[] figures = {
+            new Rectangle(){ 
+                Name = "Прямоугольник",
+                Color = System.Drawing.Color.DarkRed,
+                Position = new System.Drawing.Point(30, 30),
+                Width = 150, Height = 60
+            },
+            new Square(){
+                Name = "Квадрат", Width = 100, Height = 100,
+                Color = System.Drawing.Color.DarkGreen,
+                Position = new System.Drawing.Point(50, 120)
+            },
+            new Circle(){
+                 Name = "Круг", R = 50,
+                Color = System.Drawing.Color.Blue,
+                Position = new System.Drawing.Point(50, 250)
+            },
+            new Triangle(){
+                Name = "Треугольник",
+                Color = System.Drawing.Color.Green,
+                Position = new System.Drawing.Point(50, 350),
+                Footing = 100, Height = 150
+            },
+        };
+    }
+}
